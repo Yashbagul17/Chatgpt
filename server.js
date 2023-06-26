5ecae59034c362906fb6bda73c9ddd07c246a7ea
Prem <premjadhav378@gmail.com>
2023-06-26 23:11:51 +0530

    new files added

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const express = require('express');
 const cors = require('cors');
 
 const {OpenAIApi,Configuration }= require('openai');
-// const bodyParser = require('body-parser');
 const configuration = new Configuration({
-    // organization:"org-i16GdI3biVetsydNnhzCYRLs",
-  apiKey: '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',
+  apiKey: "",
 });
 const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors())
 app.use(express.json())
-
 app.post('/', async (req, res) => {
   try {
 const {message} = req.body;
-console.log(message)
    const response = await openai.createCompletion({
   model: "text-davinci-003",
   prompt: `${message}`,
@@ -27,8 +23,8 @@ console.log(message)
       message: response.data.choices[0].text
     });
   } catch (error) {
+    console.log(error.message);
     res.status(500).send(error || 'New Error');
   }
 })
-
-app.listen(5000, () => console.log('AI server started '));
+app.listen(1000, () => console.log('AI server started '));
